4105fbd6399f5fd8bf18cad22ac0f0305e8c9b94
Mauli Saxena <maulisaxena.16@gmail.com>
2026-01-14 19:23:58 +0530

    created auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt=require("jsonwebtoken")
+const verifyToken=(req,res,next)=>{
+    let token
+    let authHeader=req.headers.authorization||authorization
+    if(authHeader && authHeader.startsWith("Bearer")){
+        token=authHeader.split(" ")[1]
+
+    }
+    if(!token){
+        return res.status(201).json({message:"No token, authorization denied"})
+    }
+    try {
+        const decode=jwt.verify(token,process.env.JWT_SECRET)
+        req.user=decode
+        console.log("The decoded user is:",req.user);
+        
+    } catch (error) {
+        res.status(400).json({message:"Token is not valid"})
+    }
+
+}
+
+
+
+
+
+
+
+module.exports=verifyToken
